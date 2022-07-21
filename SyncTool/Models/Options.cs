34fe86ce33e2c65f42f225a7d24d4f742da85c1a
Dan <dan@ddev.ch>
2022-07-21 18:13:00 +0200

    Removed Help text as it is crashing

--- SyncTool/Models/Options.cs
+++ SyncTool/Models/Options.cs
@@ -7,10 +7,10 @@ namespace SyncTool.Models
 {
     public class Options
     {
-        [Option('s', "source", Required = false, ResourceType = typeof(DirectoryInfo), HelpText ="Source Directory")]
+        [Option('s', "source", Required = false, ResourceType = typeof(DirectoryInfo))]
         public DirectoryInfo Source { get; set; } = new DirectoryInfo(Environment.CurrentDirectory);
 
-        [Option('t', "target", Required = false, ResourceType = typeof(DirectoryInfo), HelpText = "Target Directory")]
+        [Option('t', "target", Required = false, ResourceType = typeof(DirectoryInfo))]
         public DirectoryInfo Target { get; set; } = new DirectoryInfo(Environment.CurrentDirectory);
 
         [Option('l', "logLevel", Required = false, ResourceType = typeof(LogLevel), Default = Logger.LogLevel.Error)]
@@ -19,16 +19,16 @@ namespace SyncTool.Models
         [Option('v', "verbose", Required = false)]
         public bool Verbose { get; set; }
 
-        [Option('d', "delete", Required = false, Default = false, ResourceType = typeof(bool), HelpText ="Should files in the target directory be deleted if noneexistent in the source (Default: false)")]
+        [Option('d', "delete", Required = false, Default = false, ResourceType = typeof(bool))]
         public bool Delete { get; set; }
 
-        [Option('c', "concurrentThreads", Required = false, Default = 50, ResourceType = typeof(int), HelpText ="Number of concurrent Threads running (default: 50)") ]
+        [Option('c', "concurrentThreads", Required = false, Default = 50, ResourceType = typeof(int)) ]
         public int Semaphore { get; set; }
 
-        [Option('m', "md5", Required = false, Default = false, ResourceType = typeof(bool), HelpText ="Use MD5 Hash to compare (might impact the performance)")]
+        [Option('m', "md5", Required = false, Default = false, ResourceType = typeof(bool))]
         public bool Md5Hash { get; set; }
 
-        [Option('w', "watch", Required = false, HelpText ="Will watch the source directory and directly sync it to the target directory")]
+        [Option('w', "watch", Required = false)]
         public bool Watch { get; set;}
     }
 }
